83238b1b8e4d7be85516bc930a9c376f595216ce
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-09-29 15:18:35 +0530

    Add files via upload

---
+++ 29Sep_lab/Arithmetic.java
@@ -0,0 +1,17 @@
+/*Q3.… Write the following code:
+1. A class named Arithmetic with a method named add that takes integers as
+parameters and returns an integer denoting their sum.
+A class named Adder that inherits from a superclass named Arithmetic  */
+
+package Lab_29Sep;
+
+public class Arithmetic{
+
+	public int add(int a,int b){	
+		int c = a+b;
+		return c;
+	}
+}
+
+
+    
