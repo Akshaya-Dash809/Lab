6759f126efc03ab453bf6bc435e399a8c5a84355
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2023-01-12 15:32:14 +0530

    Add files via upload

---
+++ 12_Jan_Lab/Hibernate_BMS/src/main/java/com/Hibernate_BMS/dao/BankDao.java
@@ -0,0 +1,8 @@
+package com.Hibernate_BMS.dao;
+
+public interface BankDao {
+	public void addAccount();
+	public void updateAccount();
+	public void deleteAccont();
+	public void fetchAccount();
+}
